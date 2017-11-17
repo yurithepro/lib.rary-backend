74be314ca9a0fc1c74c6ef79426774b1188a2d56
root <root@yuri.fios-router.home>
2017-11-16 21:08:47 -0500

    fixed bugs

--- routes/search.js
+++ routes/search.js
@@ -10,6 +10,8 @@ var router = express.Router();
 //THINGS TO CONSIDER
 //Add a counter that counts login attempts and blacklists repeat offenders
 
+//add db.close()...
+
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +26,8 @@ function format(req, res, next){
 	} else
 		res.locals.lim = 100;
 	res.locals.searchResult = null;
+	//install responses
+	res.locals.r = req.query.r;
 
 	next();
 }
